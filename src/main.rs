d158d09a7ae5e27fe8c8d0eb71b4bb8e64172e98
Tathya Garg <tathya.garg@gmail.com>
2025-01-25 22:58:14 +0530

    Rewrite

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,3 @@
-use std::env;
-
-mod lexer;
-mod parser;
-
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        println!("Usage: {} <file_name>", args[0]);
-        return;
-    }
-
-    let file_name = &args[1];
-    let contents = std::fs::read_to_string(file_name).expect("Failed to read file");
-
-    let mut parser = parser::Parser::new(lexer::Lexer::new(contents).lex());
-    println!("{:?}", parser.parse());
-    println!("{:?}", parser.parse());
+pub fn main() {
+    ()
 }
